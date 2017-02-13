c0f9807432773004dd2626e45f4a5aeae42bace2
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-02-13 12:27:23 -0500

    Semana 2, video 4/4

--- app/src/main/java/com/example/mauricio/tutorialandroid/MainActivity.java
+++ app/src/main/java/com/example/mauricio/tutorialandroid/MainActivity.java
@@ -4,20 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "Los Mensajes";
 
+    EditText etEntrada;
+    TextView tvSalida;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEntrada = (EditText) findViewById(R.id.etEntrada);
+        tvSalida = (TextView) findViewById(R.id.tvSalida);
     }
 
     public void onClickElBoton(View view) {
-        Toast.makeText(this,"Click en el boton ElBoton", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "El boton");
+        String entrada = etEntrada.getText().toString();
+        tvSalida.setText(entrada);
     }
 }
