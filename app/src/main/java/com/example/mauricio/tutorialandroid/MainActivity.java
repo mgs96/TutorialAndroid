f8a90d42968bfa3687e74590ccc3a0bee1e8133b
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-02-13 12:02:28 -0500

    Semana 2, video 3/4

--- app/src/main/java/com/example/mauricio/tutorialandroid/MainActivity.java
+++ app/src/main/java/com/example/mauricio/tutorialandroid/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.mauricio.tutorialandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "Los Mensajes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickElBoton(View view) {
+        Toast.makeText(this,"Click en el boton ElBoton", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "El boton");
     }
 }
